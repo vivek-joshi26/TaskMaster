454f811d4ffe4a4fc24ac0168a27d4f8ed58beff
vivek-joshi26 <vivek.jsh2@gmail.com>
2021-08-08 17:31:56 -0700

    Added files for heroku deployment

--- app.py
+++ app.py
@@ -89,6 +89,7 @@ def stopServer():
     
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0', port= 8070)
+    app.run(debug=True)
+    # 'for running on local' app.run(debug=True,host='0.0.0.0', port= 8070)
 
 
